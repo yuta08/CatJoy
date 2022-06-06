3dd6d45030ff572abbc9d5ce9002d5c905beb40c
yuta08 <69028022+yuta08@users.noreply.github.com>
2022-06-06 23:37:32 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     author="yuta ishii",
     author_email="s2022052@stu.musashino-u.ac.jp",
     description = 'This package visualizes the happiness of cats based on Japanese traditional lore.',
-    long_description='In Japan, there is a saying, "When the autumn rains fall, the cat\'s face becomes three feet long. This means that the cat\'s face is longer and more joyful on rainy days than on sunny days in autumn, because the temperature is higher on rainy days. We assume that the more it rains, the more pleased the cat will be. Of course, this is limited to autumn. If it is autumn now, this will help. There is a lot going on in the world right now. But it does not matter to cats. If cats are happy, the world will be peaceful.',
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/yuta08/CatJoy",
     project_urls={
