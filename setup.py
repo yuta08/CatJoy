0482c910e86979489321afdcd8a4309c81a070fc
yuta <>
2022-06-06 23:10:46 +0900

    change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="CatJoy",
+    name="JoyCat",
     version="0.0.1",
     author="yuta ishii",
     author_email="s2022052@stu.musashino-u.ac.jp",
